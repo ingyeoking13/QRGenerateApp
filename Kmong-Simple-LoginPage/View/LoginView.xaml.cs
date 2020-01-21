b9b95c8160d73bbc79af8023fa8d6e8c3677b3c5
ingyeoking13 <ingyeoking13@gmail.com>
2020-01-22 03:00:06 +0900

    hi

---
+++ Kmong-Simple-LoginPage/View/LoginView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Kmong_Simple_LoginPage.View
+{
+    public partial class LoginView : UserControl
+    {
+        private Action _action {get;}
+        public LoginView(Action action)
+        {
+            InitializeComponent();
+            _action = action;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _action?.Invoke();
+        }
+    }
+}
---
+++ Kmong-Simple-LoginPage/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Kmong_Simple_LoginPage.ViewModel
+{
+    public static class INotifyPropertyChangedExtension
+    {
+        public static void SetValue<T>(this INotifyPropertyChanged instance, T value, )
+    }
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string propName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
