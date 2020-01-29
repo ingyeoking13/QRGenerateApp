223363b16001bfa28f4f59b0058d45e64d14ab5d
Jeong Yohan <ingyeoking13@gmail.com>
2020-01-29 12:20:05 +0900

    commit -hi

---
+++ CreateClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CreateClass
+{
+    class Program
+    {
+
+        static T CreateClass<T>() where T : new()
+        {
+            return (T)Activator.CreateInstance(typeof(T));
+        }
+        static void Main(string[] args)
+        {
+            Person a = CreateClass<Person>();
+            Person b = CreateClass<Teacher>();
+            Animal c = CreateClass<Animal>();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Person
+    {
+
+    }
+    class Teacher:Person
+    {
+
+    }
+    class Animal
+    { }
+
+}
