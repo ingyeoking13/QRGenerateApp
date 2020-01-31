43e8a8aad4ac65b600489e29032b1749850b1a63
Jeong Yohan <ingyeoking13@gmail.com>
2020-01-31 11:42:19 +0900

    Partial Test

---
+++ Partial Class Property Test/HI.cs
@@ -0,0 +1,11 @@
+﻿namespace Partial_Class_Property_Test
+{
+    partial class Program
+    {
+        private int myNumber;
+        public Program()
+        {
+            MyProperty = 2;
+        }
+    }
+}
---
+++ Partial Class Property Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Partial_Class_Property_Test
+{
+    partial class Program
+    {
+        public int MyProperty { get; set; }
+        static void Main(string[] args)
+        {
+            var myP = new Program();
+            myP.myNumber = 5;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
